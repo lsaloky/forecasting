0ca5d1d5fc04a0471f1988dad1de04e14c8c69f3
Lubos Saloky <Lubos.Saloky@gmail.com>
2024-06-02 08:18:25 +0200

    Preparation for Kp

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,52 +1,42 @@
 import json
 import numpy
 import pandas
-import time
 
 import matplotlib.pyplot as plt
 
 SIMULATIONS_COUNT = 1000
 
 config = json.load(open('config.json', 'r'))
-prediction = input(f'Choose prediction ({', '.join(config.keys())}): ')
-periods = input('Periods to predict: ')
+prediction = 'KP'
+periods = '120'
 
-# Load data from CSV
-filename = config[prediction]['file'].format(int(time.time()) - int(config[prediction].get('timespan_days', 0)) * 24 * 3600, int(time.time()))
-data = pandas.read_csv(filename)
+# Load data from space delimited file, ignore first rows(comments)
+filename = config[prediction]['file']
+data = pandas.read_fwf(filename, skiprows=29)
 
-xcolumn = config[prediction]['x_column_name']
-ycolumn = config[prediction]['y_column_name']
+# Remove data with value Kp of -1
+xcolumn = 0
+ycolumn = 8
+for index in range(len(data) - 1, -1, -1):
+    if data.values[index][ycolumn] < 0:
+        data.drop(index, inplace = True, axis = 0)
 
 # Simulation - for every day, add a random daily diff from the observed values
 simulation = [[0 for i in range(SIMULATIONS_COUNT)] for j in range(int(periods))]
 for simulation_index in range(SIMULATIONS_COUNT):
     for day_index in range(int(periods)):
         # Use the last known observed value to start simulation. 
-        previous = simulation[day_index - 1][simulation_index] if day_index > 0 else data[ycolumn].values[-1]
-        index = numpy.random.randint(0, data[ycolumn].size - 1)
-        simulation[day_index][simulation_index] = previous + data[ycolumn][index + 1] - data[ycolumn][index]
+        previous = simulation[day_index - 1][simulation_index] if day_index > 0 else data.values[-1][ycolumn]
+        index = numpy.random.randint(0, len(data) - 1)
+        simulation[day_index][simulation_index] = previous + data.values[index + 1][ycolumn] - data.values[index][ycolumn]
+        if simulation[day_index][simulation_index] < 0:
+            simulation[day_index][simulation_index] = 0
+        if simulation[day_index][simulation_index] > 9:
+            simulation[day_index][simulation_index] = 9
 
-# For each simulation, get 25th percentile, median and 75th percentile for every day
-lower_bound = []
-median = []
-upper_bound = []
-for day_index in range(int(periods)):
-    lower_bound.append(numpy.percentile(simulation[day_index], 25))
-    median.append(numpy.percentile(simulation[day_index], 50))
-    upper_bound.append(numpy.percentile(simulation[day_index], 75))
+# Interpret the data somehow and create a forecast. Issue: limited data, containing and extraordinary event.
+# More data, for multiple years: https://www.spaceweatherlive.com/en/auroral-activity/top-50-geomagnetic-storms/year/2024.html 
 
-# Prepare future values in a new DataFrame
-forecast = pandas.DataFrame({
-    xcolumn: pandas.date_range(start=data[xcolumn].iloc[-1], periods=int(periods), freq=config[prediction]['frequency']),
-    'lower': lower_bound,
-    ycolumn: median,
-    'upper': upper_bound
-})
-forecast[xcolumn] = [str(i.date()) for i in pandas.to_datetime(forecast[xcolumn])]
-
-# Plot forecast
-plt.plot(data[xcolumn], data[ycolumn], label='Observed')
-plt.plot(forecast[xcolumn], median, label='Forecast')
-plt.fill_between(forecast[xcolumn], forecast['lower'], forecast['upper'], color='gray', alpha=0.2)
+# Plot observed data
+plt.plot(range(len(data)), data.iloc[:,ycolumn - 1], label='Observed')
 plt.show()
